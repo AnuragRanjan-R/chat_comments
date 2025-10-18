288372eacf0881acc3b292c88b3a5788733ba3dd
Anurag Ranjan <152269758+AnuragRanjan-R@users.noreply.github.com>
2025-10-18 14:32:37 +0530

    cors

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 # Adjust allow_origins for your production frontend URL
 origins = [
     "http://localhost",
-    "http://localhost:3000",
+    "https://chat-comments-fnt-to3v.vercel.app/",
 ]
 
 app.add_middleware(
